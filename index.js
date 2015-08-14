1caa75103619cb5f7a45a5ab83b7d566a1d8ea26
Michael Angstadt <mangstadt@cbancnetwork.com>
2015-08-14 17:37:41 -0500

    modified cli call.

--- index.js
+++ index.js
@@ -73,8 +73,7 @@ function wkhtmltopdf(input, options, callback) {
     var child = spawn(args[0], args.slice(1));
   } else {
     // this nasty business prevents piping problems on linux
-    console.log("call made is::", '/bin/sh ', args.join(' '));
-    var child = spawn('/bin/sh ', args.join(' '));
+    var child = spawn(args.join(' '));
   }
 
   // call the callback with null error when the process exits successfully
